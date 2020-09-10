af2836c7c8d138f4d900b89aa62baa962f34c007
Trigg Summs <stsumms@gmail.com>
2020-09-10 15:47:22 -0500

    Blog Section List and Add is complete, posting branch for reference

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    // Note, we must Open() the connection, the "using" block doesn't do that for us.
+                    conn.Open();
+
+                    // We must "use" commands too.
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        // Here we setup the command with the SQL we want to execute before we execute it.
+                        cmd.CommandText = @"SELECT id, Title, URL FROM Blog";
+
+                        // A list to hold the rooms we retrieve from the database.
+                        List<Blog> blogs = new List<Blog>();
+
+                        // Execute the SQL in the database and get a "reader" that will give us access to the data.
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        // Read() will return true if there's more data to read
+                        while (reader.Read())
+                        {
+
+                            
+                                Blog blog = new Blog()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    Title = reader.GetString(reader.GetOrdinal("Title")),
+                                    Url = reader.GetString(reader.GetOrdinal("Url"))
+                                    
+                                };
+                            // ...and add that room object to our list.
+                            blogs.Add(blog);
+                        }
+                            // The "ordinal" is the numeric position of the column in the query results.
+                            //  For our query, "Id" has an ordinal value of 0 and "Name" is 1.
+           
+                        // We should Close() the reader. Unfortunately, a "using" block won't work here.
+                        reader.Close();
+
+                        // Return the list of rooms who whomever called this method.
+                        return blogs;
+                    }
+                }
+            }
+            /// <summary>
+            ///  Returns a single room with the given id.
+            /// </summary>
+        }
+
+        ///BEGINING OF GET BY ID_---------------------------------------------------------------------------------------------------------
+        public Blog Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT b.Id AS BlogId,
+                                               b.Title,
+                                               b.Url,
+                                               t.Id AS TagId,
+                                               t.Name
+                                          FROM Blog b 
+                                               LEFT JOIN BlogTag bt on b.Id = bt.BlogId
+                                               LEFT JOIN Tag t on t.Id = bt.TagId
+                                         WHERE b.id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    Blog blog = null;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (blog == null)
+                        {
+                            blog = new Blog()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Url = reader.GetString(reader.GetOrdinal("Url"))
+                            };
+                        }
+
+                        if (!reader.IsDBNull(reader.GetOrdinal("TagId")))
+                        {
+                            blog.Tags.Add(new Tag()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("TagId")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                            });
+                        }
+                    }
+
+                    reader.Close();
+
+                    return blog;
+                }
+            }
+        }
+
+        public void Insert(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog (Title, Url )
+                                                     VALUES (@title, @url)";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+                    
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Blog 
+                                           SET Title = @title,
+                                               Url = @url
+                                              
+                                         WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Blog WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void InsertTag(Blog blog, Tag tag)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO BlogTag (BlogId, TagId)
+                                                       VALUES (@Id, @tagId)";
+                    cmd.Parameters.AddWithValue("@blogId", blog.Id);
+                    cmd.Parameters.AddWithValue("@tagId", tag.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteTag(int blogId, int tagId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM BlogTag 
+                                         WHERE BlogId = @blogid AND 
+                                               TagId = @tagId";
+                    cmd.Parameters.AddWithValue("@blogId", blogId);
+                    cmd.Parameters.AddWithValue("@tagId", tagId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -9,8 +9,20 @@ namespace TabloidCLI.UserInterfaceManagers
 
         public IUserInterfaceManager Execute()
         {
+            Console.WriteLine("Welcome...Press any Key to contine");
+            Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine(@"
+//   __ __    ___  _      _       ___   __      __    __    ___  _         __   ___   ___ ___    ___      ______   ___       ___    ____       ______    ___    ___  ______  __ __          ______  __ __    ___        ___  _        ___     __  ______  ____   ____     __      ___ ___   ____  __ __  __ __    ___  ___ ___  __   _____        __   ___   ____    _____  ___   _        ___       ____  ____   ____   __ 
+//  |  |  |  /  _]| |    | |     /   \ |  |    |  |__|  |  /  _]| |       /  ] /   \ |   |   |  /  _]    |      | /   \     |   \  |    \     |      |  /  _]  /  _]|      ||  |  |        |      ||  |  |  /  _]      /  _]| |      /  _]   /  ]|      ||    \ |    |   /  ]    |   |   | /    ||  |  ||  |  |  /  _]|   |   ||  | / ___/       /  ] /   \ |    \  / ___/ /   \ | |      /  _]     /    ||    \ |    \ |  |
+//  |  |  | /  [_ | |    | |    |     ||  |    |  |  |  | /  [_ | |      /  / |     || _   _ | /  [_     |      ||     |    |    \ |  D  )    |      | /  [_  /  [_ |      ||  |  |        |      ||  |  | /  [_      /  [_ | |     /  [_   /  / |      ||  D  ) |  |   /  /     | _   _ ||  o  ||  |  ||  |  | /  [_ | _   _ ||_ |(   \_       /  / |     ||  _  |(   \_ |     || |     /  [_     |  o  ||  o  )|  o  )|  |
+//  |  _  ||    _]| |___ | |___ |  O  ||__|    |  |  |  ||    _]| |___  /  /  |  O  ||  \_/  ||    _]    |_|  |_||  O  |    |  D  ||    /     |_|  |_||    _]|    _]|_|  |_||  _  |        |_|  |_||  _  ||    _]    |    _]| |___ |    _] /  /  |_|  |_||    /  |  |  /  /      |  \_/  ||     ||  ~  ||  _  ||    _]|  \_/  |  \| \__  |     /  /  |  O  ||  |  | \__  ||  O  || |___ |    _]    |     ||   _/ |   _/ |__|
+//  |  |  ||   [_ |     ||     ||     | __     |  `  '  ||   [_ |     |/   \_ |     ||   |   ||   [_       |  |  |     |    |     ||    \  __   |  |  |   [_ |   [_   |  |  |  |  |          |  |  |  |  ||   [_     |   [_ |     ||   [_ /   \_   |  |  |    \  |  | /   \_     |   |   ||  _  ||___, ||  |  ||   [_ |   |   |     /  \ |    /   \_ |     ||  |  | /  \ ||     ||     ||   [_     |  _  ||  |   |  |    __ 
+//  |  |  ||     ||     ||     ||     ||  |     \      / |     ||     |\     ||     ||   |   ||     |      |  |  |     |    |     ||  .  \|  |  |  |  |     ||     |  |  |  |  |  |          |  |  |  |  ||     |    |     ||     ||     |\     |  |  |  |  .  \ |  | \     |    |   |   ||  |  ||     ||  |  ||     ||   |   |     \    |    \     ||     ||  |  | \    ||     ||     ||     |    |  |  ||  |   |  |   |  |
+//  |__|__||_____||_____||_____| \___/ |__|      \_/\_/  |_____||_____| \____| \___/ |___|___||_____|      |__|   \___/     |_____||__|\_||__|  |__|  |_____||_____|  |__|  |__|__|          |__|  |__|__||_____|    |_____||_____||_____| \____|  |__|  |__|\_||____| \____|    |___|___||__|__||____/ |__|__||_____||___|___|      \___|     \____| \___/ |__|__|  \___| \___/ |_____||_____|    |__|__||__|   |__|   |__|
+//                                                                                                                                                                                                                                                                                                                                                                                                                          
+");
             Console.WriteLine("Main Menu");
-
             Console.WriteLine(" 1) Journal Management");
             Console.WriteLine(" 2) Blog Management");
             Console.WriteLine(" 3) Author Management");
@@ -24,7 +36,7 @@ namespace TabloidCLI.UserInterfaceManagers
             switch (choice)
             {
                 case "1": throw new NotImplementedException();
-                case "2": throw new NotImplementedException();
+                case "2": return new BlogManager(this, CONNECTION_STRING);
                 case "3": return new AuthorManager(this, CONNECTION_STRING);
                 case "4": throw new NotImplementedException();
                 case "5": return new TagManager(this, CONNECTION_STRING);
