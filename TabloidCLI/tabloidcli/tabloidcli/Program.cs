4f4619e3b263dfeb1f2333466e5b457c06722d0f
Rob Mixon <mixonroberth@gmail.com>
2020-09-10 16:10:23 -0500

    working on journal

---
+++ TabloidCLI/tabloidcli/tabloidcli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tabloidcli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
